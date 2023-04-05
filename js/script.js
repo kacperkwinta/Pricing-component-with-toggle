d33ecdb4671387f16ec9c4492c062fd98f6cc730
turbobaguettes <kaspersky.kwinta@gmail.com>
2023-04-05 18:44:44 +0200

    js pricing mechanics added

--- js/script.js
+++ js/script.js
@@ -1,16 +1,33 @@
 "use strict";
-console.log(123);
 
 //////////////////////////////
-/// HEADER BTN FOR PRICING
+/// BTN FOR PRICING
 //////////////////////////////
 const btnToggle = document.querySelector(".toggle");
 const btnToggleDot = document.querySelector(".dot");
 const pricingMonthlyText = document.querySelector(".monthly");
 const pricingAnnuallyText = document.querySelector(".annually");
+const prices = document.querySelectorAll(".card__price");
 
-btnToggle.addEventListener("click", () => {
+btnToggle.addEventListener("click", function () {
 	btnToggleDot.classList.toggle("dot--move");
 	pricingAnnuallyText.classList.toggle("annually--active");
 	pricingMonthlyText.classList.toggle("monthly--active");
+	changePrice();
 });
+
+function changePrice() {
+	const pricesYear = [`$199.99`, `$249.99`, `$399.99`];
+	const pricesMonth = [`$19.99`, `$24.99`, `$39.99`];
+
+	if (pricingMonthlyText.classList.contains("monthly--active")) {
+		for (let i = 0; i < prices.length; i++) {
+			prices[i].innerHTML = [pricesMonth[i]];
+		}
+	}
+	if (pricingAnnuallyText.classList.contains("annually--active")) {
+		for (let i = 0; i < prices.length; i++) {
+			prices[i].innerHTML = [pricesYear[i]];
+		}
+	}
+}
